9b8753564e170dc9819f9a7e6a38737dfbe70dfe
JohnnyDallas <vfscorbandallas@gmail.com>
2022-12-20 09:40:03 +0700

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'dcceeaf4-6029-40b3-8ae1-9e0939418d02';
+export const MODAL_CLOSE_SEC = 2.5;
